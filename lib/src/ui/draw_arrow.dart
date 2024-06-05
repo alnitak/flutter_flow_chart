8f98fb9492c5f16b9c18f16dc5b0c877a2e6d7f7
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2024-06-05 13:01:34 +0200

    added tension to make segments curved

--- lib/src/ui/draw_arrow.dart
+++ lib/src/ui/draw_arrow.dart
@@ -3,48 +3,88 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flow_chart/flutter_flow_chart.dart';
 import 'package:flutter_flow_chart/src/ui/segment_handler.dart';
 
+/// Arrow style enumeration
 enum ArrowStyle {
+  /// A curved arrow which points nicely to each handlers
   curve,
+
+  /// A segmented line where pivot points can be added and curvature between
+  /// them can be adjusted with a tension.
   segmented,
+
+  /// A rectangular shaped line.
   rectangular,
 }
 
 /// Arrow parameters used by [DrawArrow] widget
 class ArrowParams extends ChangeNotifier {
-  /// Arrow thickness
-  double thickness;
-
-  double headRadius;
-
-  /// Arrow color
-  final Color color;
-
-  /// The start position alignment
-  final Alignment startArrowPosition;
-
-  /// The end position alignment
-  final Alignment endArrowPosition;
-
-  /// The tail length of the arrow
-  double _tailLength;
-
-  /// The style of the arrow
-  ArrowStyle? style;
-
+  ///
   ArrowParams({
     this.thickness = 1.7,
     this.headRadius = 6,
     double tailLength = 25.0,
     this.color = Colors.black,
     this.style,
+    this.tension = 1.0,
     this.startArrowPosition = Alignment.centerRight,
     this.endArrowPosition = Alignment.centerLeft,
   }) : _tailLength = tailLength;
 
+  ///
+  factory ArrowParams.fromMap(Map<String, dynamic> map) {
+    return ArrowParams(
+      thickness: map['thickness'] as double,
+      headRadius: map['headRadius'] as double? ?? 6.0,
+      tailLength: map['tailLength'] as double? ?? 25.0,
+      color: Color(map['color'] as int),
+      style: ArrowStyle.values[map['style'] as int? ?? 0],
+      tension: map['tension'] as double? ?? 1,
+      startArrowPosition: Alignment(
+        map['startArrowPositionX'] as double,
+        map['startArrowPositionY'] as double,
+      ),
+      endArrowPosition: Alignment(
+        map['endArrowPositionX'] as double,
+        map['endArrowPositionY'] as double,
+      ),
+    );
+  }
+
+  ///
+  factory ArrowParams.fromJson(String source) =>
+      ArrowParams.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  /// Arrow thickness.
+  double thickness;
+
+  /// The radius of arrow tip.
+  double headRadius;
+
+  /// Arrow color.
+  final Color color;
+
+  /// The start position alignment.
+  final Alignment startArrowPosition;
+
+  /// The end position alignment.
+  final Alignment endArrowPosition;
+
+  /// The tail length of the arrow.
+  double _tailLength;
+
+  /// The style of the arrow.
+  ArrowStyle? style;
+
+  /// The curve tension for pivot points when using [ArrowStyle.segmented].
+  /// 0 means no curve on segments.
+  double tension;
+
+  ///
   ArrowParams copyWith({
     double? thickness,
     Color? color,
     ArrowStyle? style,
+    double? tension,
     Alignment? startArrowPosition,
     Alignment? endArrowPosition,
   }) {
@@ -52,11 +92,13 @@ class ArrowParams extends ChangeNotifier {
       thickness: thickness ?? this.thickness,
       color: color ?? this.color,
       style: style ?? this.style,
+      tension: tension ?? this.tension,
       startArrowPosition: startArrowPosition ?? this.startArrowPosition,
       endArrowPosition: endArrowPosition ?? this.endArrowPosition,
     );
   }
 
+  ///
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'thickness': thickness,
@@ -64,6 +106,7 @@ class ArrowParams extends ChangeNotifier {
       'tailLength': _tailLength,
       'color': color.value,
       'style': style?.index,
+      'tension': tension,
       'startArrowPositionX': startArrowPosition.x,
       'startArrowPositionY': startArrowPosition.y,
       'endArrowPositionX': endArrowPosition.x,
@@ -71,29 +114,10 @@ class ArrowParams extends ChangeNotifier {
     };
   }
 
-  factory ArrowParams.fromMap(Map<String, dynamic> map) {
-    return ArrowParams(
-      thickness: map['thickness'].toDouble(),
-      headRadius: map['headRadius']?.toDouble() ?? 6.0,
-      tailLength: map['tailLength']?.toDouble() ?? 25.0,
-      color: Color(map['color'] as int),
-      style: ArrowStyle.values[map['style'] as int? ?? 0],
-      startArrowPosition: Alignment(
-        map['startArrowPositionX'].toDouble(),
-        map['startArrowPositionY'].toDouble(),
-      ),
-      endArrowPosition: Alignment(
-        map['endArrowPositionX'].toDouble(),
-        map['endArrowPositionY'].toDouble(),
-      ),
-    );
-  }
-
+  ///
   String toJson() => json.encode(toMap());
 
-  factory ArrowParams.fromJson(String source) =>
-      ArrowParams.fromMap(json.decode(source) as Map<String, dynamic>);
-
+  ///
   void setScale(double currentZoom, double factor) {
     thickness = thickness / currentZoom * factor;
     headRadius = headRadius / currentZoom * factor;
@@ -101,37 +125,51 @@ class ArrowParams extends ChangeNotifier {
     notifyListeners();
   }
 
+  ///
   double get tailLength => _tailLength;
 }
 
 /// Notifier to update arrows position, starting/ending points and params
 class DrawingArrow extends ChangeNotifier {
   DrawingArrow._();
+
+  /// Singleton instance of this.
   static final instance = DrawingArrow._();
 
+  /// Arrow parameters.
   ArrowParams params = ArrowParams();
-  setParams(ArrowParams params) {
+
+  /// Sets the parameters.
+  void setParams(ArrowParams params) {
     this.params = params;
     notifyListeners();
   }
 
+  /// Starting arrow offset.
   Offset from = Offset.zero;
-  setFrom(Offset from) {
+
+  ///
+  void setFrom(Offset from) {
     this.from = from;
     notifyListeners();
   }
 
+  /// Ending arrow offset.
   Offset to = Offset.zero;
-  setTo(Offset to) {
+
+  ///
+  void setTo(Offset to) {
     this.to = to;
     notifyListeners();
   }
 
+  ///
   bool isZero() {
     return from == Offset.zero && to == Offset.zero;
   }
 
-  reset() {
+  ///
+  void reset() {
     params = ArrowParams();
     from = Offset.zero;
     to = Offset.zero;
@@ -142,48 +180,63 @@ class DrawingArrow extends ChangeNotifier {
 /// Draw arrow from [srcElement] to [destElement]
 /// using [arrowParams] parameters
 class DrawArrow extends StatefulWidget {
+  ///
+  DrawArrow({
+    required this.srcElement,
+    required this.destElement,
+    required List<Pivot> pivots,
+    super.key,
+    ArrowParams? arrowParams,
+    this.onTap,
+    this.onLongPress,
+    this.onSecondaryTap,
+    this.onSecondaryLongPress,
+  })  : arrowParams = arrowParams ?? ArrowParams(),
+        pivots = PivotsNotifier(pivots);
+
+  ///
   final ArrowParams arrowParams;
+
+  ///
   final FlowElement srcElement;
+
+  ///
   final FlowElement destElement;
-  final Function(
+
+  ///
+  final void Function(
     BuildContext context,
     Offset clickPosition,
     FlowElement srcElement,
     FlowElement destElement,
   )? onTap;
-  final Function(
+
+  ///
+  final void Function(
     BuildContext context,
     Offset clickPosition,
     FlowElement srcElement,
     FlowElement destElement,
   )? onLongPress;
-  final Function(
+
+  ///
+  final void Function(
     BuildContext context,
     Offset clickPosition,
     FlowElement srcElement,
     FlowElement destElement,
   )? onSecondaryTap;
-  final Function(
+
+  ///
+  final void Function(
     BuildContext context,
     Offset clickPosition,
     FlowElement srcElement,
     FlowElement destElement,
   )? onSecondaryLongPress;
 
+  ///
   final PivotsNotifier pivots;
-
-  DrawArrow({
-    super.key,
-    ArrowParams? arrowParams,
-    required this.srcElement,
-    required this.destElement,
-    this.onTap,
-    this.onLongPress,
-    this.onSecondaryTap,
-    this.onSecondaryLongPress,
-    required List<Pivot> pivots,
-  })  : arrowParams = arrowParams ?? ArrowParams(),
-        pivots = PivotsNotifier(pivots);
 
   @override
   State<DrawArrow> createState() => _DrawArrowState();
@@ -206,14 +259,14 @@ class _DrawArrowState extends State<DrawArrow> {
     super.dispose();
   }
 
-  _elementChanged() {
+  void _elementChanged() {
     if (mounted) setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    Offset from = Offset.zero;
-    Offset to = Offset.zero;
+    var from = Offset.zero;
+    var to = Offset.zero;
 
     from = Offset(
       widget.srcElement.position.dx +
@@ -236,79 +289,68 @@ class _DrawArrowState extends State<DrawArrow> {
               ((widget.arrowParams.endArrowPosition.y + 1) / 2)),
     );
 
-    Offset tapPosition = Offset.zero;
-    Offset secondaryTapPosition = Offset.zero;
+    var tapPosition = Offset.zero;
+    var secondaryTapPosition = Offset.zero;
     return GestureDetector(
       onTapDown: (details) => tapPosition = details.localPosition,
       onSecondaryTapDown: (details) =>
           secondaryTapPosition = details.localPosition,
       onTap: () {
-        if (widget.onTap != null) {
-          widget.onTap!(
-            context,
-            tapPosition,
-            widget.srcElement,
-            widget.destElement,
-          );
-        }
+        widget.onTap?.call(
+          context,
+          tapPosition,
+          widget.srcElement,
+          widget.destElement,
+        );
       },
       onLongPress: () {
-        if (widget.onLongPress != null) {
-          widget.onLongPress!(
-            context,
-            tapPosition,
-            widget.srcElement,
-            widget.destElement,
-          );
-        }
+        widget.onLongPress?.call(
+          context,
+          tapPosition,
+          widget.srcElement,
+          widget.destElement,
+        );
       },
       onSecondaryTap: () {
-        if (widget.onSecondaryTap != null) {
-          widget.onSecondaryTap!(
-            context,
-            secondaryTapPosition,
-            widget.srcElement,
-            widget.destElement,
-          );
-        }
+        widget.onSecondaryTap?.call(
+          context,
+          secondaryTapPosition,
+          widget.srcElement,
+          widget.destElement,
+        );
       },
       onSecondaryLongPress: () {
-        if (widget.onSecondaryLongPress != null) {
-          widget.onSecondaryLongPress!(
-            context,
-            secondaryTapPosition,
-            widget.srcElement,
-            widget.destElement,
-          );
-        }
+        widget.onSecondaryLongPress?.call(
+          context,
+          secondaryTapPosition,
+          widget.srcElement,
+          widget.destElement,
+        );
       },
       child: RepaintBoundary(
-        child: Builder(builder: (context) {
-          return CustomPaint(
-            painter: ArrowPainter(
-              params: widget.arrowParams,
-              from: from,
-              to: to,
-              pivots: widget.pivots.value,
-            ),
-            child: Container(),
-          );
-        }),
+        child: Builder(
+          builder: (context) {
+            return CustomPaint(
+              painter: ArrowPainter(
+                params: widget.arrowParams,
+                from: from,
+                to: to,
+                pivots: widget.pivots.value,
+              ),
+              child: Container(),
+            );
+          },
+        ),
       ),
     );
   }
 }
 
 /// Paint the arrow connection taking in count the
-/// [params.startArrowPosition] and [params.endArrowPosition] alignment
+/// [ArrowParams.startArrowPosition] and
+/// [ArrowParams.endArrowPosition] alignment.
 class ArrowPainter extends CustomPainter {
-  final ArrowParams params;
-  final Offset from;
-  final Offset to;
-  final Path path = Path();
-  final lines = [];
-  final List<Pivot> pivots;
-
+  ///
   ArrowPainter({
     required this.params,
     required this.from,
@@ -316,41 +358,71 @@ class ArrowPainter extends CustomPainter {
     List<Pivot>? pivots,
   }) : pivots = pivots ?? [];
 
+  ///
+  final ArrowParams params;
+
+  ///
+  final Offset from;
+
+  ///
+  final Offset to;
+
+  ///
+  final Path path = Path();
+
+  ///
+  final List<List<Offset>> lines = [];
+
+  ///
+  final List<Pivot> pivots;
+
   @override
   void paint(Canvas canvas, Size size) {
-    final Paint paint = Paint();
-    paint.strokeWidth = params.thickness;
+    final paint = Paint()..strokeWidth = params.thickness;
+
     if (params.style == ArrowStyle.curve) {
       drawCurve(canvas, paint);
     } else if (params.style == ArrowStyle.segmented) {
-      drawLine(canvas, paint);
+      drawLine();
     } else if (params.style == ArrowStyle.rectangular) {
       drawRectangularLine(canvas, paint);
     }
 
     canvas.drawCircle(to, params.headRadius, paint);
 
-    paint.color = params.color;
-    paint.style = PaintingStyle.stroke;
+    paint
+      ..color = params.color
+      ..style = PaintingStyle.stroke;
     canvas.drawPath(path, paint);
   }
 
-  void drawLine(Canvas canvas, Paint paint) {
-    List<Offset> points = [];
-    points.add(from);
-
+  /// Draw a segmented line with a tension between points.
+  void drawLine() {
+    final points = [from];
     for (final pivot in pivots) {
       points.add(pivot.pivot);
     }
-
     points.add(to);
 
-    for (int i = 0; i < points.length - 1; i++) {
-      canvas.drawLine(points[i], points[i + 1], paint);
-      lines.add([points[i], points[i + 1]]);
+    path.moveTo(points.first.dx, points.first.dy);
+
+    for (var i = 0; i < points.length - 1; i++) {
+      final p0 = (i > 0) ? points[i - 1] : points[0];
+      final p1 = points[i];
+      final p2 = points[i + 1];
+      final p3 = (i != points.length - 2) ? points[i + 2] : p2;
+
+      final cp1x = p1.dx + (p2.dx - p0.dx) / 6 * params.tension;
+      final cp1y = p1.dy + (p2.dy - p0.dy) / 6 * params.tension;
+
+      final cp2x = p2.dx - (p3.dx - p1.dx) / 6 * params.tension;
+      final cp2y = p2.dy - (p3.dy - p1.dy) / 6 * params.tension;
+
+      path.cubicTo(cp1x, cp1y, cp2x, cp2y, p2.dx, p2.dy);
     }
   }
 
+  /// Draw a rectangular line
   void drawRectangularLine(Canvas canvas, Paint paint) {
     // calculating offsetted pivot
     var pivot1 = Offset(from.dx, from.dy);
@@ -360,25 +432,30 @@ class ArrowPainter extends CustomPainter {
       pivot1 = Offset(from.dx, from.dy - params.tailLength);
     }
 
-    Offset pivot2 = Offset(to.dx, pivot1.dy);
+    final pivot2 = Offset(to.dx, pivot1.dy);
 
-    canvas.drawLine(from, pivot1, paint);
-    canvas.drawLine(pivot1, pivot2, paint);
-    canvas.drawLine(pivot2, to, paint);
+    path
+      ..moveTo(from.dx, from.dy)
+      ..lineTo(pivot1.dx, pivot1.dy)
+      ..lineTo(pivot2.dx, pivot2.dy)
+      ..lineTo(to.dx, to.dy);
+
     lines.addAll([
       [from, pivot2],
-      [pivot2, to]
+      [pivot2, to],
     ]);
   }
 
+  /// Draws a curve starting/ending the handler linearly from the center
+  /// of the element.
   void drawCurve(Canvas canvas, Paint paint) {
-    double distance = 0;
+    var distance = 0.0;
 
-    double dx = 0;
-    double dy = 0;
+    var dx = 0.0;
+    var dy = 0.0;
 
-    Offset p0 = Offset(from.dx, from.dy);
-    Offset p4 = Offset(to.dx, to.dy);
+    final p0 = Offset(from.dx, from.dy);
+    final p4 = Offset(to.dx, to.dy);
     distance = (p4 - p0).distance / 3;
 
     // checks for the arrow direction
@@ -392,7 +469,7 @@ class ArrowPainter extends CustomPainter {
     } else if (params.startArrowPosition.y < 0) {
       dy = -distance;
     }
-    Offset p1 = Offset(from.dx + dx, from.dy + dy);
+    final p1 = Offset(from.dx + dx, from.dy + dy);
     dx = 0;
     dy = 0;
 
@@ -407,17 +484,18 @@ class ArrowPainter extends CustomPainter {
     } else if (params.endArrowPosition.y < 0) {
       dy = -distance;
     }
-    Offset p3 = params.endArrowPosition == const Alignment(0.0, 0.0)
+    final p3 = params.endArrowPosition == Alignment.center
         ? Offset(to.dx, to.dy)
         : Offset(to.dx + dx, to.dy + dy);
-    Offset p2 = Offset(
+    final p2 = Offset(
       p1.dx + (p3.dx - p1.dx) / 2,
       p1.dy + (p3.dy - p1.dy) / 2,
     );
 
-    path.moveTo(p0.dx, p0.dy);
-    path.conicTo(p1.dx, p1.dy, p2.dx, p2.dy, 1.0);
-    path.conicTo(p3.dx, p3.dy, p4.dx, p4.dy, 1.0);
+    path
+      ..moveTo(p0.dx, p0.dy)
+      ..conicTo(p1.dx, p1.dy, p2.dx, p2.dy, 1)
+      ..conicTo(p3.dx, p3.dy, p4.dx, p4.dy, 1);
   }
 
   @override
@@ -474,33 +552,39 @@ class ArrowPainter extends CustomPainter {
   }
 }
 
+/// Notifier for pivot points.
 class PivotsNotifier extends ValueNotifier<List<Pivot>> {
+  ///
   PivotsNotifier(super.value) {
     for (final pivot in value) {
       pivot.addListener(notifyListeners);
     }
   }
 
-  add(Pivot pivot) {
+  /// Add a pivot point.
+  void add(Pivot pivot) {
     value.add(pivot);
     pivot.addListener(notifyListeners);
     notifyListeners();
   }
 
-  remove(Pivot pivot) {
+  /// Remove a pivot point.
+  void remove(Pivot pivot) {
     value.remove(pivot);
     pivot.removeListener(notifyListeners);
     notifyListeners();
   }
 
-  insert(int index, Pivot pivot) {
+  /// Insert a pivot point.
+  void insert(int index, Pivot pivot) {
     value.insert(index, pivot);
     pivot.addListener(notifyListeners);
     notifyListeners();
   }
 
-  removeAt(int index) {
-    (value.removeAt(index)).removeListener(notifyListeners);
+  /// Remove a pivot point by its index.
+  void removeAt(int index) {
+    value.removeAt(index).removeListener(notifyListeners);
     notifyListeners();
   }
 }
