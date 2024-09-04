2a3e224cbef1d06d94819a4a7243a330be418e7f
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2024-09-04 20:27:12 +0200

    fix position not updated sometime after drag end

--- lib/src/flow_chart.dart
+++ lib/src/flow_chart.dart
@@ -258,7 +258,7 @@ class _FlowChartState extends State<FlowChart> {
           // Draw elements
           for (int i = 0; i < widget.dashboard.elements.length; i++)
             ElementWidget(
-              key: ValueKey(widget.dashboard.elements.elementAt(i).id),
+              key: UniqueKey(),
               dashboard: widget.dashboard,
               element: widget.dashboard.elements.elementAt(i),
               onElementPressed: widget.onElementPressed == null
