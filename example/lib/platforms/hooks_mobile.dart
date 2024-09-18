a7830688f7fc929da65a021868fc156a54a2109c
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2024-09-18 08:42:48 +0200

    chore

--- example/lib/platforms/hooks_mobile.dart
+++ example/lib/platforms/hooks_mobile.dart
@@ -8,15 +8,16 @@ import 'package:path_provider/path_provider.dart' as path;
 /// Save dashboard to file
 Future<void> saveDashboard(Dashboard dashboard) async {
   final appDocDir = await path.getApplicationDocumentsDirectory();
-  dashboard.saveDashboard('${appDocDir.path}/FLOWCHART-311.json');
+  dashboard.saveDashboard('${appDocDir.path}/FLOWCHART.json');
 }
 
 /// Load dashboard from file
 Future<void> loadDashboard(Dashboard dashboard) async {
   final appDocDir = await path.getApplicationDocumentsDirectory();
-  dashboard.loadDashboard('${appDocDir.path}/FLOWCHART-311.json');
+  dashboard.loadDashboard('${appDocDir.path}/FLOWCHART.json');
 }
 
+/// Pick image
 Future<Uint8List?> pickImageBytes() async {
   final pickResult = await FilePicker.platform.pickFiles(
     type: FileType.image,
